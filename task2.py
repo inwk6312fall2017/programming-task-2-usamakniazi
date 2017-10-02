63ff04d9b3125b8fcd2ce98e6119bcaf20813134
usamakniazi <usamakniazi@dal.ca>
2017-10-02 13:14:26 -0300

    task2

---
+++ task2.py
@@ -0,0 +1,16 @@
+def read(filename, content)
+
+
+f = open("path/to/file.txt")
+for line in f:
+    print line
+f.close()
+
+
+
+#list of crimes and database
+L =[-63.58794187664143	GLORIA MCCLUSKEY AVE	1430	ASSAUT]
+
+
+
+print(L)
